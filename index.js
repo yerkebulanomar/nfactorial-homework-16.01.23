796a4a191b001887c00c84aefcce211273f7ca3a
yerkebulan.omar <yerkebulan.omar@gmail.com>
2023-01-19 11:51:30 +0600

    last save

--- index.js
+++ index.js
@@ -54,22 +54,34 @@
 
 // 6
 
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
 
-let users = [ vasya, petya, masha ];
+// let users = [ vasya, petya, masha ];
 
-let usersMapped = users.map(item => ({
-    fullName: `${item.name} ${item.surname}`,
-    id: item.id
-  }));
+// let usersMapped = users.map(item => ({
+//     fullName: `${item.name} ${item.surname}`,
+//     id: item.id
+//   }));
 
-// usersMapped = [
-//   { fullName: "Вася Пупкин", id: 1 },
-//   { fullName: "Петя Иванов", id: 2 },
-//   { fullName: "Маша Петрова", id: 3 }
-// ]
+// // usersMapped = [
+// //   { fullName: "Вася Пупкин", id: 1 },
+// //   { fullName: "Петя Иванов", id: 2 },
+// //   { fullName: "Маша Петрова", id: 3 }
+// // ]
 
-alert( usersMapped[0].id ) // 1
-alert( usersMapped[0].fullName ) // Вася Пупкин
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // Вася Пупкин
+
+// 7
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+let arr = [ vasya, petya, masha ];
+
+let getAverageAge = arr.map(item => item.age)
+
+alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
